0a5a455b3bcda226c8a2bfe1a0ff0402a087d21d
Diego Lirio <diegolirio.dl@gmail.com>
2020-02-01 18:26:01 -0300

    Navigator route and ListView Product

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: ProductForm()
-      )
-);
+void main() => runApp(MaterialApp(home: ProductApp()));
 
 class ProductApp extends StatelessWidget {
   @override
@@ -16,47 +12,58 @@ class ProductApp extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
+        onPressed: () {
+          final Future<Product> future =
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return ProductForm();
+          }));
+          future.then((productRegistered) {
+            debugPrint("=======================");
+            debugPrint("$productRegistered");
+          });
+        },
       ),
     );
   }
 }
 
 class ProductForm extends StatelessWidget {
-
-  final TextEditingController _nameTextEditingController = TextEditingController();
-  final TextEditingController _priceTextEditingController = TextEditingController();
+  final TextEditingController _nameTextEditingController =
+      TextEditingController();
+  final TextEditingController _priceTextEditingController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Register your Product")),
-        body: Column(
-          children: <Widget>[
-            Edit(
-              controller: _nameTextEditingController,
-              label: "Name",
-              hint: "Ex: IPhone Xs",
-            ),
-            Edit(
-              controller: _priceTextEditingController,
-              label: "Price",
-              hint: "9,99",
-              icon: Icons.monetization_on,
-              textInputType: TextInputType.number,
-            ),
-            RaisedButton(
-              child: Text('Confirmar'),
-              onPressed: () => this._saveProduct(context),
-            )
-          ],
-        ),
+      appBar: AppBar(title: Text("Register your Product")),
+      body: Column(
+        children: <Widget>[
+          Edit(
+            controller: _nameTextEditingController,
+            label: "Name",
+            hint: "Ex: IPhone Xs",
+          ),
+          Edit(
+            controller: _priceTextEditingController,
+            label: "Price",
+            hint: "9,99",
+            icon: Icons.monetization_on,
+            textInputType: TextInputType.number,
+          ),
+          RaisedButton(
+            child: Text('Confirmar'),
+            onPressed: () => this._saveProduct(context),
+          )
+        ],
+      ),
     );
   }
 
-  _saveProduct(context) {
+  _saveProduct(BuildContext context) {
     String name = _nameTextEditingController.text;
     double price = double.tryParse(_priceTextEditingController.text);
-    if(price == null || name == "") {
+    if (price == null || name == "") {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -73,18 +80,15 @@ class ProductForm extends StatelessWidget {
                 ),
               ],
             );
-          }
-      );
+          });
       return;
     }
     final product = Product(name, price);
-    debugPrint('$product');
+    Navigator.pop(context, product);
   }
-
 }
 
 class Edit extends StatelessWidget {
-
   final TextEditingController controller;
   final String label;
   final String hint;
@@ -95,7 +99,7 @@ class Edit extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return             Padding(
+    return Padding(
       padding: const EdgeInsets.all(16.0),
       child: TextField(
         controller: this.controller,
@@ -103,9 +107,9 @@ class Edit extends StatelessWidget {
           fontSize: 24,
         ),
         decoration: InputDecoration(
-            labelText: this.label,
-            hintText: this.hint,
-            icon: this.icon != null ? Icon(this.icon) : null,
+          labelText: this.label,
+          hintText: this.hint,
+          icon: this.icon != null ? Icon(this.icon) : null,
         ),
         keyboardType: this.textInputType,
       ),
@@ -113,32 +117,24 @@ class Edit extends StatelessWidget {
   }
 }
 
-
 class ProductList extends StatelessWidget {
+
+  final List<Product> _products = List();
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-        ProductItem(Product("Iphone X", 3000.99)),
-        ProductItem(Product("Notebook", 59000.99)),
-      ],
+
+    return ListView.builder(
+        itemCount: _products.length,
+        itemBuilder: (context, index) {
+            final product = _products[index];
+            return ProductItem(product);
+        }
     );
   }
 }
 
 class ProductItem extends StatelessWidget {
-
   final Product _product;
 
   ProductItem(this._product);
@@ -147,16 +143,14 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text(this._product._name),
-          subtitle: Text(this._product._price.toString()),
-        )
-    );
+      leading: Icon(Icons.monetization_on),
+      title: Text(this._product._name),
+      subtitle: Text(this._product._price.toString()),
+    ));
   }
 }
 
 class Product {
-
   final String _name;
   final double _price;
 
@@ -166,6 +160,4 @@ class Product {
   String toString() {
     return 'Product{_name: $_name, _price: $_price}';
   }
-
-
 }
