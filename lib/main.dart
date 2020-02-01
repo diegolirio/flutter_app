f9f4f1ee2ef9d1955257e296f4a5daa95ef98f91
Diego Lirio <diegolirio.dl@gmail.com>
2020-02-01 13:02:21 -0300

    Refactor Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,48 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-
-        )
+          body: ProductList(),
+          appBar: AppBar(
+              title: Text("Products"),
+          ),
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+          ),
+        ),
       )
+);
+
+
+class ProductList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Product("Iphone X", 3000.99),
+        Product("Notebook", 59000.99),
+      ],
     );
+  }
+}
+
+class Product extends StatelessWidget {
+
+  final String _name;
+  final double _price;
+
+  Product(this._name, this._price);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(this._name),
+          subtitle: Text(this._price.toString()),
+        )
+    );
+  }
+
+
+}
